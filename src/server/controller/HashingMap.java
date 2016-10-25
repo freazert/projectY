cde171eb995afcc44c6213bd8bc302b71251e041
jeroen van den broeck <jeroen_vdb1@hotmail.com>
2016-10-25 17:30:45 +0200

    HashingMap functionality

--- src/server/controller/HashingMap.java
+++ src/server/controller/HashingMap.java
@@ -1,68 +1,42 @@
 package server.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.HashMap;
-import java.util.Map;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 //import marshal2.Jef;
-
+@XmlRootElement
 public class HashingMap {
-	private Hashing hash;
+	@XmlElement
+	public HashMap<Integer, String> hashMap;
 	
 	public HashingMap(Hashing hash)
 	{
-		this.hash = hash;
-		createHashMap();
+		this.hashMap = new HashMap<Integer, String>();
+		addRecord(hash, "192.168.1.1");
 	}
 	
-	private void createHashMap()
+	public HashingMap()
 	{
-		Map hashMap = new HashMap();
-		
-		hashMap.put(hash.getHash(), "192.168.1.1");
-		
-		System.out.println("size: " + hashMap.size());
-		
-		
-		//File file = new File("D:/school/hashmap.xml");
-		try {
-		/*	JAXBContext jc = JAXBContext.newInstance( Array.class);
-			Unmarshaller u = jc.createUnmarshaller();
-			
-			Marshaller m = jc.createMarshaller();
-		       
-			FileOutputStream fileOut =
-			         new FileOutputStream("D:/school/hashmap.xml");
-			//m.marshal(persoon, fileOut);
-			String[] hashArr = hashMap.entrySet().toArray();
-			m.marshal(hashMap,  
-				fileOut);
-			/*ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(persoon);
-         	out.close();*/
-         	/*fileOut.close();
-         	System.out.println("Serialized data is saved in /tmp/employee.xml");
-         	
-         	Jef persoon2 = (Jef)u.unmarshal(new File("D:/school/sch-IW_EI/employee.xml"));
-         	
-         	System.out.println(persoon2.getName());
-         	System.out.println(persoon2.getAge());
-         	System.out.println(persoon2.getCapacity());	
-	         
-         	/*FileInputStream fileIn = 
-	        		 new FileInputStream("D:/school/SCH-IW_EI/employee.ser");
-         	ObjectInputStream oIn = new ObjectInputStream(fileIn);
-         	Jef persoon2 = (Jef)oIn.readObject();
-         	System.out.println(persoon2.getName());*/
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		this.hashMap = new HashMap<Integer, String>();
 	}
+	
+	public void addRecord(Hashing hash, String ip)
+	{
+		this.hashMap.put(hash.getHash(), ip);
+	}
+	
+	public void removeRecord(Hashing hash)
+	{
+		this.hashMap.remove(hash.getHash());
+	}
+	
+	public String getIp(Hashing hash)
+	{
+		return this.hashMap.get(hash.getHash()).toString();
+	}
+	
+	
 }
