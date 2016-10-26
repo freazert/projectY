d5bbae32a763fc0094c99c429eab8494edef8bda
jeroen van den broeck <jeroen_vdb1@hotmail.com>
2016-10-26 14:42:52 +0200

    functionality for getting ip from hashingMap

--- src/server/controller/HashingMap.java
+++ src/server/controller/HashingMap.java
@@ -1,42 +1,83 @@
 package server.controller;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-
 
 //import marshal2.Jef;
 @XmlRootElement
 public class HashingMap {
 	@XmlElement
 	public HashMap<Integer, String> hashMap;
-	
-	public HashingMap(Hashing hash)
-	{
+
+	public HashingMap(Hashing hash) {
 		this.hashMap = new HashMap<Integer, String>();
 		addRecord(hash, "192.168.1.1");
 	}
-	
-	public HashingMap()
-	{
+
+	public HashingMap() {
 		this.hashMap = new HashMap<Integer, String>();
 	}
-	
-	public void addRecord(Hashing hash, String ip)
-	{
+
+	public void addRecord(Hashing hash, String ip) {
 		this.hashMap.put(hash.getHash(), ip);
 	}
-	
-	public void removeRecord(Hashing hash)
-	{
+
+	public void removeRecord(Hashing hash) {
 		this.hashMap.remove(hash.getHash());
 	}
-	
-	public String getIp(Hashing hash)
-	{
+
+	public String getIp(Hashing hash) {
 		return this.hashMap.get(hash.getHash()).toString();
 	}
-	
-	
+
+	public String getNext(Hashing hash)
+	{
+		Map.Entry<Integer, String> record = null;
+		Map.Entry<Integer, String> highestRecord = null;
+		Map.Entry<Integer, String> lowestRecord = null;
+		int fileHash = hash.getHash();
+		
+		Iterator it = this.hashMap.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry<Integer, String> pair = (Map.Entry)it.next();
+	        if( record == null) {
+	        	System.out.println("record is null");
+	        	record = pair;
+	        	highestRecord = pair;
+	        	lowestRecord = pair;
+	        } else {
+	        	System.out.println("hash: " + hash.getHash());
+	        	
+	        	//set lowest and highest hashes
+	        	if(highestRecord.getKey() < pair.getKey()) {
+	        		highestRecord = pair;
+	        	} else if(lowestRecord.getKey() > pair.getKey()) {
+	        		lowestRecord = pair;
+	        	}
+	        	
+	        	//check for new record
+	        	if(fileHash < pair.getKey() && record.getKey() > pair.getKey()) {
+	        		record = pair;
+	        	}
+	        }
+	        
+	        it.remove(); // avoids a ConcurrentModificationException
+	    }
+	    
+	    // if fileHash is higher than highest hash, take lowest hash
+	    if(fileHash > highestRecord.getKey()) {
+	    	record = lowestRecord;
+	    }
+	    
+		return record.getValue();
+	}
+
+	private void sort() {
+
+	}
+
 }
--- src/server/controller/Main.java
+++ src/server/controller/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -16,12 +17,24 @@ public class Main {
 		Hashing hash = new Hashing("YorickDeBock");
 		HashingMap hmap;
 
-		System.out.println(hash.getHash());
+		/*System.out.println(hash.getHash());
 		System.out.println(hash.getName());
+		
+		/*hmap = new HashingMap();
+		hmap.addRecord(hash, "192.168.1.1");
+		hash = new Hashing("Jeroen");
+		hmap.addRecord(hash, "192.168.1.2");
+		hash = new Hashing("Kris");
+		hmap.addRecord(hash, "192.168.1.3");*/
+		
+		
 
 		try {
 			hmap = xmlToObject();
-			System.out.println(hmap.getIp(new Hashing("YorickDeBock")));
+			System.out.println(hmap.getNext(new Hashing("test.txt")));
+			//System.out.println(hmap.toString());
+			//objectToXml(hmap);
+			//System.out.println(hmap.getIp(new Hashing("YorickDeBock")));
 		} catch (JAXBException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,8 +62,10 @@ public class Main {
 		try {
 			fileOut = new FileOutputStream("D:/school/hashMap.xml");
 			Marshaller marshaller = jaxbContext.createMarshaller();
+			//StringWriter wr = new StringWriter();
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			marshaller.marshal(object, fileOut);
+			//System.out.println(wr);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
