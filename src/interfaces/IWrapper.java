f712df52076715543169783d1622f18232b59315
freazert <frysfros@hotmail.com>
2016-11-09 12:09:26 +0100

    Added to wrapper hashing name

--- src/interfaces/IWrapper.java
+++ src/interfaces/IWrapper.java
@@ -3,10 +3,13 @@ package interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+import server.controller.Hashing;
+
 public interface IWrapper extends Remote {
 	String getFileNode(String name) throws RemoteException;
 	String getIp(String name) throws RemoteException;
 	int createNode(String name, String ip) throws RemoteException;
 	int removeNode(String name) throws RemoteException;
+	Hashing getHash(String name);
 
 }
