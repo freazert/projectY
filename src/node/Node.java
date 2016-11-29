dca65e3d5e95a98bc0a24e3d9c23a4b2d1ebc886
freazert <frysfros@hotmail.com>
2016-11-29 11:14:59 +0100

    node shutdown aanpassing

--- src/node/Node.java
+++ src/node/Node.java
@@ -1,5 +1,7 @@
 package node;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
@@ -88,6 +90,46 @@ public class Node {
 		System.out.println("previous node: " + this.prevNode);
 	}
 	
+	 public void shutdown()
+		{
+			try {
+				DatagramPacket packet;
+				IWrapper obj = (IWrapper) Naming.lookup("//" + "192.168.1.15" + "/getWrapper");
+				obj.removeNode(this.prevNode);
+				
+				socket = new DatagramSocket(2000);
+				
+				String toSend = "previous: " + previous;
+				byte[] buf = new byte[toSend.getBytes().length];
+				buf = toSend.getBytes();
+				
+	            packet = new DatagramPacket(buf,buf.length,obj.getIp(next),portMulticasting);
+	            socket.send(packet);
+	            
+	            
+	            
+	            
+				obj.getIp(previuos);
+				
+				String toSend = "next: " + next;
+				byte[] buf = new byte[toSend.getBytes().length];
+				buf = toSend.getBytes();
+				
+	            packet = new DatagramPacket(buf,buf.length,obj.getIp(previous),portMulticasting);
+	            socket.send(packet);
+				
+				
+			} catch (MalformedURLException e) {
+				// ODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (RemoteException e) {
+				// ODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NotBoundException e) {
+				// ODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	
 
 }
