1c541ec1e014eb8da0df6de399414bf9def7802b
jeroen van den broeck <jeroen van den broeck>
2016-12-06 11:07:57 +0100

    changes node for replication

--- src/node/SendFileThread.java
+++ src/node/SendFileThread.java
@@ -48,6 +48,9 @@ public class SendFileThread extends Thread {
 				//receive
 				TCPSend sendFile = new TCPSend(5555);
 				sendFile.sendFile(file.getName());
+				
+				ReceiveFileThread rft = new ReceiveFileThread();
+				rft.start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
