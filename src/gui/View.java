d7edcf4a5e2817fbdce3d77b585c0e162686a8c4
Jordy <developerjordy@gmail.com>
2017-01-07 19:28:51 +0100

    codedocs gui package

--- src/gui/View.java
+++ src/gui/View.java
@@ -10,7 +10,12 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-
+/**
+ * 
+ * View Class containing the actual GUI with wich the user interacts.
+ * Extends JFrame so GUI elements can be added to it.
+ *
+ */
 public class View extends JFrame{
 	
 	private Model model;
@@ -18,6 +23,14 @@ public class View extends JFrame{
 	private JButton openButton, removeButton, downloadButton;
 
 	
+	/**
+	 * The constructor of the gui's VIEW ( V in MVC pattern ).
+	 * Instanciates the JScrollPane that uses the Model's JList list in it's constructor, and other GUI elements like buttons.
+	 * Also adds the elements (buttons and JScrollPane) to the JFrame
+	 * 
+	 * @param model
+	 * 				the gui's Model (M in MVC), containing the JList list that is used in the JScrollFrame
+	 */
 	public View(Model model)
 	{
 		this.model = model;
@@ -44,16 +57,37 @@ public class View extends JFrame{
 
 	}
 
+	/**
+	 * creates an addActionListener(ActionListener event) on the openButton. 
+	 * This function is used in the controller.
+	 * 
+	 * @param event
+	 * 				The click event on the openButton
+	 */
 	public void openButtonListener(ActionListener event)
 	{
 		this.openButton.addActionListener(event);
 	}
 
+	/**
+	 * creates an addActionListener(ActionListener event) on the removeButton. 
+	 * This function is used in the controller.
+	 * 
+	 * @param event
+	 * 				The click event on the removeButton
+	 */
 	public void removeButtonListener(ActionListener event)
 	{
 		this.removeButton.addActionListener(event);
 	}	
 	
+	/**
+	 * creates an addActionListener(ActionListener event) on the dowloadButton. 
+	 * This function is used in the controller.
+	 * 
+	 * @param event
+	 * 				The click event on the downloadButton
+	 */
 	public void downloadButtonListener(ActionListener event)
 	{
 		this.downloadButton.addActionListener(event);
